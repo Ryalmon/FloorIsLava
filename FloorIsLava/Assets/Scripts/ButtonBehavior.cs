7f9a27003dcd78fb6c62a152b7655f2f96895205
troberts2 <troberts2@mail.bradley.edu>
2023-02-15 12:39:51 -0600

    door and button art implement

--- FloorIsLava/Assets/Scripts/ButtonBehavior.cs
+++ FloorIsLava/Assets/Scripts/ButtonBehavior.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class ButtonBehavior : MonoBehaviour
 {
-
+    public bool active = false;
+    //private GameController gc;
+    private GameController gc;
+    private SpriteRenderer sr;
+    public Sprite pressedButton;
+    // Start is called before the first frame update
     GameManager gm;
     private void Start()
     {
         gm = FindObjectOfType<GameManager>();
+        sr.sprite = pressedButton;
         //gm.AddB(this.gameObject);
        
     }
-    public bool active = false;
-    //private GameController gc;
-    // Start is called before the first frame update
+
+
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.CompareTag("Player")){
             if(!active){
                 active = true;
-                GetComponent<SpriteRenderer>().color = Color.red;
                 gm.RemoveB(this.gameObject);
-                //gc.buttonsPressed++; 
-                //Debug.Log("buttons pressed: " + gc.buttonsPressed);
+                sr.sprite = pressedButton;
             }
 
         }
